c72ae6ce59ac366a844f0c8a1ae2fe54b3b1cd6f
dreadwing5 <sachinspyk@outlook.com>
2021-08-20 22:56:17 +0530

    Consuming Promises

--- 11-Asynchronous/script.js
+++ 11-Asynchronous/script.js
@@ -3,47 +3,6 @@
 const btn = document.querySelector(".btn-country");
 const countriesContainer = document.querySelector(".countries");
 
-///////////////////////////////////////
-/* const getCountryData = (country) => {
-  const request = new XMLHttpRequest(); //Old school way of doing AJAX request
-  request.open("GET", `https://restcountries.eu/rest/v2/name/${country}`);
-  request.send();
-
-  // When the request is done, we can use the responseText property to get the response
-  request.addEventListener("load", function () {
-    const [data] = JSON.parse(this.responseText);
-
-    console.log(data);
-
-    // Create a new country element
-    const html = `
-  <article class="country">
-    <img class="country__img" src=${data.flag} />
-    <div class="country__data">
-      <h3 class="country__name">${data.name}</h3>
-      <h4 class="country__region">${data.region}</h4>
-      <p class="country__row"><span>👫</span>${(
-        +data.population / 1000000
-      ).toFixed(1)}</p>
-      <p class="country__row"><span>🗣️</span>${data.languages[0].name}</p>
-      <p class="country__row"><span>💰</span>${data.currencies[0].name}</p>
-     </div>
-  </article>
-    
-  `;
-
-    countriesContainer.insertAdjacentHTML("beforeend", html);
-    countriesContainer.style.opacity = 1;
-  });
-};
-
-getCountryData("portugal");
-getCountryData("usa");
-getCountryData("germany"); */
-
-//////////////////////////////-----********-------//////////////////////////////
-
-//Callback hell
 const renderCountry = (data, className = "") => {
   const html = `
 <article class="country ${className}">
@@ -62,8 +21,18 @@ const renderCountry = (data, className = "") => {
   countriesContainer.insertAdjacentHTML("beforeend", html);
   countriesContainer.style.opacity = 1;
 };
+///////////////////////////////////////
+/* 
 
-const getCountryAndNeighbor = (country) => {
+getCountryData("portugal");
+getCountryData("usa");
+getCountryData("germany"); */
+
+//////////////////////////////-----********-------//////////////////////////////
+
+//Callback hell
+
+/* const getCountryAndNeighbor = (country) => {
   const request = new XMLHttpRequest(); //Old school way of doing AJAX request
   //AJAX call country 1
   request.open("GET", `https://restcountries.eu/rest/v2/name/${country}`);
@@ -108,4 +77,27 @@ setTimeout(() => {
       console.log("3 seconds passed");
     }, 1000);
   }, 1000);
-}, 1000);
+}, 1000); */
+
+//////////////////////////////-----********-------//////////////////////////////
+//Promises
+
+// const getCountryData = function (country) {
+//   fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+//     .then(function (response) {
+//       console.log(response);
+//       return response.json(); //json method is available on promise object, json() is also a promise
+//     })
+//     .then(function (data) {
+//       console.log(data);
+//       renderCountry(data[0]);
+//     });
+// };
+
+//Simplified version
+const getCountryData = function (country) {
+  fetch(`https://restcountries.eu/rest/v2/name/${country}`)
+    .then((response) => response.json())
+    .then((data) => renderCountry(data[0]));
+};
+getCountryData("portugal");
